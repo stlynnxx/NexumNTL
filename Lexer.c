cd585fe249ce391a09af44bb441d2a18a37e5176
stlynnxx <stevie@eclecticelectronics.com>
2026-03-10 22:09:38 -0500

    Super light lexer work

--- Lexer.c
+++ Lexer.c
@@ -41,7 +41,8 @@ void *loadNexFile(FILE *fp, MemoryFileSplit *split) {
    split->mainArray[size] = '\0';
 
 }
-char associations(char wC[50], char fileArray[50], int tracker) {
+
+char associations(char wC[50], char fileArray[2000], int tracker) {
 
     // Whenever associations is called initally wC will be on
     // the first association's opening name token
@@ -49,11 +50,14 @@ char associations(char wC[50], char fileArray[50], int tracker) {
     bool associatorBool = false;
     bool nameTokenBool = false;
     int letterCounter = 0;
+    int size;
+    int exportSize;
     int associatorLetterCounter = 0;
     int associatiorStartPoint;
     char workingAssociations[50];
     int associationsSizes[100];
     char workingAssociators[200];
+    char export[250];
     wC[0] = fileArray[tracker];
     if (wC == nameToken)
     {
@@ -65,6 +69,8 @@ char associations(char wC[50], char fileArray[50], int tracker) {
     for (int i = 0; i < alphasLength; i++) {
         // This is the same loop I'm using for the alpha check
         // for the memory key with the bool swapped out
+
+
         if (wC == nameToken | wC[i] == alphas[i]) {
             wC[i] = fileArray[tracker];
             if (wC == nameToken) {
@@ -123,8 +129,15 @@ char associations(char wC[50], char fileArray[50], int tracker) {
         }
 
     }
+    size = sizeof(workingAssociations) / sizeof(workingAssociations[0]);
+    for (int i = 0; i < size ; i++) {
+        export[i] = workingAssociations[i];
+    }
+    exportSize = sizeof(export) / sizeof(export[0]);
+    exportSize = exportSize + 1;
+    export[exportSize] = tracker;
     // Tracker at this point should be on the closing brace of the line in question
-    return workingAssociations[0], tracker;
+    return export[0];
 }
 
 void crawler(FILE *fp) {
@@ -168,7 +181,8 @@ void crawler(FILE *fp) {
                       // Just because even if it's only a pass for a nameToken at this point we're
                       // Likely reading a memoryKey
                         memoryKeyBool = true;
-
+                        // The idea here is that the while loop will run until memkeybool
+                        // gets flipped and THEN if wC == nameToken runs
                         while (memoryKeyBool == true) {
                             if (wC[i] == alphas[i])
                             {
@@ -191,6 +205,7 @@ void crawler(FILE *fp) {
                     }
 
             }
+            // I'm unsure about using these multi char tokens
             if (wC[0] == openingSeq[0])
                 {
                     tracker++;
@@ -202,9 +217,9 @@ void crawler(FILE *fp) {
                     wC[0] = memoryFileSplit.mainArray[tracker];
                     while (wC[0] == alphas[0] | wC == nameToken)
                         {
-                            associations(wC, fileArray ,tracker);
+                            associations(wC, memoryFileSplit.mainArray, tracker);
                             tracker++;
-                            wC[0] = fileArray[tracker];
+                            wC[0] = memoryFileSplit.mainArray[tracker];
                         }
 
                 }
@@ -215,107 +230,7 @@ void crawler(FILE *fp) {
 
 
 }
-void rawler(FILE *fp) {
 
-        for (int i = 0; i < sizeof(memoryFileSplit.mainArray); i++)
-        {
-            /* Here the first char of memfilesplit.mainarray is loaded into workingChar[0]
-              letterCounter is also sitting at 0 */
-            fileArray[0] = memoryFileSplit.mainArray[i];
-            // Don't forget that fileArray is the entirety of the file at this moment
-            wC[0] = fileArray[0];
-            if (wC == openBraceToken) {
-                /* If we are thinking of wC as a 'selector' moving across the file
-                Char by Char then this next line advances the selector */
-                wC[0] = fileArray[1];
-                tracker++;
-                /* We won't exactly be needing tracker at this moment but it's still good to
-                 * keep it updated */
-                if (wC == nameToken) {
-                    // This will need to advance workingChar until another nameToken is hit
-                    // And then append everything between to workingMemKeys
-                    wC[0] = fileArray[2];
-                    tracker++;
-                    nameTokenPoint = tracker;
-                    // this is the loop that will be doing the letter check.
-                    // Alphas and alphas length are in SymbolTable.h
-                    // This is also where we get the length of the memory key
-                    // via letterCounter.
-                    for (int i = 0; i < alphasLength; i++) {
-                        // I'm not super convinced that this is exactly how I
-                        // want to handle letter checking for real as it seems
-                        // prone to unwanted results/unnecessary loops.
-                        // But also maybe I'm overthinking it
-                        if (wC[i] != alphas[i]) {
-                            memoryKeyBool = false;
-                        }
-                        if (workingCheck[i] == alphas[i]) {
-                            memoryKeyBool = true;
-                            workingMemKeys[i] = fileArray[i];
-                            letterCounter++;
-                            tracker++;
-                        }
-                    }
-                    /* letterCounter gets us the length of the Memory Key.
-                     * From here we can go from the point of one char after the initial
-                     * nametoken (namePointToken is that exact index)
-                    and add letterCounter and that range will hold the memory key and we can append
-                    it to a seperate array.
-                    if nameTokenPoint = 5 and letterCounter = 6, then 5-11 is our MemoryKeyRange
-
-                    */
-                    // If we're thinking of tracker as a sort of mirror for the imagined selector of
-                    // fileArray then this statement moves tracker onto the first char after the last letter of
-                    // the memory key, which means it should return a name token.
-                    tracker++;
-                    wC[0] = fileArray[tracker];
-                    if (wC == nameToken) {
-                        tracker++;
-                        wC[0] = fileArray[tracker];
-                    }
-                    // Right now we aren't using a comma in the .nex
-                    /*
-                    if (wC == comma) {
-                        tracker++;
-                        tracker++;
-                        wC[0] = fileArray[tracker];
-                    }*/
-
-
-
-
-                    if (wC == nameToken) {
-                        //This is where we will begin associator logic. We need to keep
-                        // in mind that it needs to be repeatable.
-
-                        // this is the start of the association loop. I've already started
-                        // a function to move all of this into but I'm feeling it out here first
-                        // and then moving it all to the function
-
-                        // We basically need to repeatedly call associations
-                        // Until we hit the end file delimiter outside of the
-                        // associations loop
-
-                        // This is the loop that is meant to call the associations
-                        // loop for all of the associations
-                        tracker++;
-                        wC[0] = fileArray[tracker];
-                        while (wC[i] == alphas[i] | wC == nameToken) {
-                            associations(wC, fileArray ,tracker);
-                            tracker++;
-                            wC[0] = fileArray[tracker];
-                        }
-
-                    }
-                    // I still need to figure out a clean end to the lexing process
-                    // here
-                }
-            }
-
-            // we will need a demlimiter to tell the association loop when to end
-        }
-    }
-}
 
 
 
